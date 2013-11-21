c91606c3cc902b3ac25f24ff0a78207402cadbb9
Loxxlie <elie@ucsc.edu>
2013-11-21 15:10:32 -0800

    Forgot this

---
+++ Game/headers/GameState.hpp
@@ -0,0 +1,26 @@
+#ifndef GAMESTATE_HPP_INCLUDED
+#define GAMESTATE_HPP_INCLUDED
+
+#include "../../engine/headers/State.hpp"
+#include "World.hpp"
+#include "Player.hpp"
+
+#include <SFML/Graphics/Sprite.hpp>
+#include <SFML/Graphics/Text.hpp>
+
+
+class GameState : public State
+{
+        public:
+                                            GameState(StateStack& stack, Context context);
+
+                virtual void                draw();
+                virtual bool                update(sf::Time dt);
+                virtual bool                handleEvent(const sf::Event& event);
+
+
+        private:
+                World                                mWorld;
+};
+
+#endif // GAMESTATE_HPP_INCLUDED
